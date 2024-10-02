743e12b15994d654a917e5fb78f2fe287272faef
mdendiardana <mdendiardana@gmail.com>
2024-10-02 11:18:00 +0700

    Fitur Baru Arcive Button

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function ArchiveButton({ id, onArchive }) {
   return (
-    <button className="note-item__archive-button" onClick={() => onArchive(id)}>
-      Archive
-    </button>
+    <div className="note-item__action">
+      <button
+        className="note-item__archive-button"
+        onClick={() => onArchive(id)}
+      >
+        Arsipkan
+      </button>
+    </div>
   );
 }
 
