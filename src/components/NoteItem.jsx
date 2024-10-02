444dc64906cd557efb366076e61481a72723027c
mdendiardana <mdendiardana@gmail.com>
2024-10-02 11:19:39 +0700

    Komponen NoteItem

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -5,18 +5,11 @@ import ArchiveButton from './Archive';
 
 function NoteItem({ title, createdAt, body, id, onDelete, onArchive }) {
   return (
-    <div className="note-item__title">
-      <NoteItemBody title={title} />
-      <div className="note-item__date">
-        <span>{createdAt}</span>
-      </div>
-      <div className="note-item__body">
-        <p>{body}</p>
-      </div>
+    <div className="note-item">
+      <NoteItemBody title={title} createdAt={createdAt} body={body} />
+
       <div className="note-item__action">
         <DeleteButton id={id} onDelete={onDelete} />
-      </div>
-      <div className="note-item__archive-button">
         <ArchiveButton id={id} onArchive={onArchive} />
       </div>
     </div>
