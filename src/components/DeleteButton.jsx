54cec0a742c367458112aeec6392b9ddff888526
mdendiardana <mdendiardana@gmail.com>
2024-10-02 14:43:57 +0700

    Fitur Komponen Button Delete

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 function DeleteButton({ id, onDelete }) {
   return (
-    <div className="note-item__action">
-      <button className="note-item__delete-button" onClick={() => onDelete(id)}>
-        Delete
-      </button>
-    </div>
+    <button className="note-item__delete-button" onClick={() => onDelete(id)}>
+      Delete
+    </button>
   );
 }
 
