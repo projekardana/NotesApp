1ead591898a3877182a22beee35bf7b1d4e37c46
mdendiardana <mdendiardana@gmail.com>
2024-10-02 08:20:28 +0700

    Komponen Note Item Body

--- src/components/NoteItemBody.jsx
+++ src/components/NoteItemBody.jsx
@@ -4,7 +4,7 @@ function NoteItemBody({ title, createdAt, body }) {
   return (
     <div className="note-item__content">
       <h3 className="note-item__title">{title}</h3>
-      <p className="note-item__createdAt">{createdAt}</p>
+      <p className="note-item__date">{createdAt}</p>
       <p className="note-item__body">{body}</p>
     </div>
   );
