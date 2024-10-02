b56482ce7cceb34908176645873b61b3bfbd0cb4
mdendiardana <mdendiardana@gmail.com>
2024-10-02 14:43:12 +0700

    Fitur Komponen Button Archive

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 function ArchiveButton({ id, onArchive }) {
   return (
-    <div className="note-item__action">
-      <button
-        className="note-item__archive-button"
-        onClick={() => onArchive(id)}
-      >
-        Arsipkan
-      </button>
-    </div>
+    <button className="note-item__archive-button" onClick={() => onArchive(id)}>
+      Arsipkan
+    </button>
   );
 }
 
