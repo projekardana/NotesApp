29e838c2cdc90bf9ec861e8c6cf07f148715ea34
mdendiardana <mdendiardana@gmail.com>
2024-10-02 11:18:59 +0700

    Fitur Baru Delete Button

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function DeleteButton({ id, onDelete }) {
   return (
-    <button className="note-item__delete-button" onClick={() => onDelete(id)}>
-      Delete
-    </button>
+    <div className="note-item__action">
+      <button className="note-item__delete-button" onClick={() => onDelete(id)}>
+        Delete
+      </button>
+    </div>
   );
 }
 
